702a7696bcb9895be5c81091b1f509ffa6f7d632
Canino Developments <ian.canino@gmail.com>
2018-04-30 18:00:30 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,32 @@
+INSERT INTO bears (
+	 ID, NAME, AGE, GENDER, COLOR, TEMPERAMENT, ALIVE)
+VALUES (1, 'Mr. Chocolate', 33, 'M', 'Magenta', 'Copacetic', 0);
+
+INSERT INTO bears (
+	 ID, NAME, AGE, GENDER, COLOR, TEMPERAMENT, ALIVE)
+VALUES (2, 'Rowdy', 33, 'M', 'Yellow', 'Agitated', 0);
+
+INSERT INTO bears (
+	 ID, NAME, AGE, GENDER, COLOR, TEMPERAMENT, ALIVE)
+VALUES (3, 'Sergeant Brown', 3, 'M', 'Magenta', 'Excited', 0);
+
+INSERT INTO bears (
+	 ID, NAME, AGE, GENDER, COLOR, TEMPERAMENT, ALIVE)
+VALUES (4, 'Melissa', 3, 'M', 'Magenta', 'Dopey', 1);
+
+INSERT INTO bears (
+	 ID, NAME, AGE, GENDER, COLOR, TEMPERAMENT, ALIVE)
+VALUES (5, 'Grinch', 21, 'F', 'Brown', 'Sad', 1);
+
+INSERT INTO bears (
+	 ID, NAME, AGE, GENDER, COLOR, TEMPERAMENT, ALIVE)
+VALUES (6, 'Tabitha', 3, 'M', 'Brown', 'Grumpy', 1);
+
+INSERT INTO bears (
+	 ID, NAME, AGE, GENDER, COLOR, TEMPERAMENT, ALIVE)
+VALUES (7, 'Wendy', 23, 'F', 'Black', 'Curious', 0);
+
+INSERT INTO bears (
+	 ID, AGE, GENDER, COLOR, TEMPERAMENT, ALIVE)
+VALUES (8, 23, 'F', 'Magenta', 'Joyful', 0);
+
