7f36e6a5df320471ddfa19b1ea07ccb3a3ed7601
sgg2123 <sgg2123@columbia.edu>
2018-04-30 10:57:13 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3 , "M", "Brown", "Docile", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 1 , "M", "Brown", "Rowdy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 5 , "F", "Black", "Docile", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 7 , "M", "Brown", "Docile", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 4 , "F", "Black", "Rowdy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 3 , "M", "Brown", "Agressive", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 2 , "F", "Black", "Docile", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 4 , "M", "Brown", "Agressive", 1);
